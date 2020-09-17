103c1ac3365852cdf0034ad5d662416426b64be0
FredSkou <44166052+FredSkou@users.noreply.github.com>
2020-09-17 15:02:13 +0200

    Update searchGoogleScript.py

--- searchGoogleScript.py
+++ searchGoogleScript.py
@@ -2,23 +2,39 @@ import googlesearch
 
 def searchGoogle(searchPhrase,hits):
     hitArray =[]
+    file_name = "link.txt"
     try:
         from googlesearch import search
     except ImportError:
         print("No module named 'google' found")
         # What Phrase to Search
     query = str(searchPhrase)
+    print("Starting Google Search For:",query)
     for hit in search(query, tld="com", num=10, stop=hits, pause=2):
         # Dont Include these Websites
         if "wikipedia" in hit:
             continue
-        elif "businessnews" in hit:
+        if "news" in hit:
             continue
-        elif "news" in hit:
+        if "view" in hit:
+            continue
+        if "search" in hit:
             continue
         else:
             hitArray.append(hit)
-    for entry in hitArray:
-        print(entry)
+    if hitArray:
+        with open(file_name,"w") as file:
+            print(f"Writing to File {file_name}")
+            for entry in hitArray:
+                entry = str(entry.split("com")[0])
+                entry = entry+"com/"
+                contactus = entry+"contactus"
+                file.writelines(entry+"\n")
+                file.writelines(contactus+"\n")
+            file.close()
+    else:
+        print("No Results")
+    print("Search Complete! Results: ",len(hitArray))
 
-searchGoogle("CRM",15)
+#Test It!
+searchGoogle("CRM",25)
